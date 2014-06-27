7e42bb028906fa8ac5048cc1144450920aa42b7e
Emily <Lemmily0@gmail.com>
2014-06-27 17:36:02 +0100

    new behaviour classes added

---
+++ core/src/com/UKC_AICS/simulation/entity/behaviours/Alignment.java
@@ -0,0 +1,18 @@
+package com.UKC_AICS.simulation.entity.behaviours;
+
+import java.util.ArrayList;
+
+import com.UKC_AICS.simulation.entity.Boid;
+import com.badlogic.gdx.math.Vector3;
+
+public class Alignment extends Behaviour {
+
+	/**
+	 * @return orientation change required to align with the groups heading.
+	 */
+	@Override
+	Vector3 act(ArrayList<Boid> list, Boid boid) {
+		return null;
+	}
+
+}
---
+++ core/src/com/UKC_AICS/simulation/entity/behaviours/Seperation.java
@@ -0,0 +1,16 @@
+package com.UKC_AICS.simulation.entity.behaviours;
+
+import java.util.ArrayList;
+
+import com.UKC_AICS.simulation.entity.Boid;
+import com.badlogic.gdx.math.Vector3;
+
+public class Seperation extends Behaviour {
+
+	@Override
+	Vector3 act(ArrayList<Boid> list, Boid boid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
