9213d26e3f397a9dca229caced5a5f8c9261dea5
James aka solf1re2 <jy1v07@gmail.com>
2014-06-27 15:54:50 +0100

    adjusted config screen size + started v0.1 class UML

--- desktop/src/com/UKS_AICS/simulation/desktop/DesktopLauncher.java
+++ desktop/src/com/UKS_AICS/simulation/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Project Savannah Simulation";
+        config.width = 1280;
+        config.height = 720;
 		new LwjglApplication(new Simulation(), config);
 	}
 }
