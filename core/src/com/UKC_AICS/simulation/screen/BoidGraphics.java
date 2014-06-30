b52c180a2078b19400dcc7cbb40e4daa094c7d0b
Ben Nicholls <nicholls.b@hotmail.co.uk>
2014-06-30 16:27:10 +0100

    Altered boid rendering - A single sprite is created which has its position updated and is drawn for ever boid in the simulation

--- core/src/com/UKC_AICS/simulation/screen/BoidGraphics.java
+++ core/src/com/UKC_AICS/simulation/screen/BoidGraphics.java
@@ -14,37 +14,56 @@ import com.UKC_AICS.simulation.entity.*;
 public class BoidGraphics {
 	
 	private SpriteBatch boidBatch;
+	private Sprite boidSprite;
 	private ObjectMap<Boid, Sprite> boidMap = new ObjectMap<Boid, Sprite>();
+	private ArrayList<Boid> boidsArray;
 	private Texture defaultTexture = new Texture(Gdx.files.internal("square.png"));
 	
 
 	
 	public void update(SpriteBatch batch){
-		Boid boid;
-		if(boidMap.size>0){
+		
+		if(boidsArray.size()>0){
 			batch.begin();
-			for(Iterator<Boid> boids = boidMap.keys(); boids.hasNext();){
+			for(Boid boid : boidsArray){
+				updateSpritePosition(boid);
+				boidSprite.draw(batch);
+			}
+		}
+		/*if(boidMap.size>0){
+			 Boid boid;
+			 for(Iterator<Boid> boids = boidMap.keys(); boids.hasNext();){
 				boid = boids.next();
 				updateSpritePosition(boid);
-				boidMap.get(boid).draw(batch);
+				boidSprite.draw(batch);
+				//boidMap.get(boid).draw(batch);
 			}
-			batch.end();
-		}
+		}*/
+		batch.end();
 	}
 	
 	public void initBoidSprites(ArrayList<Boid> arrayList){
+		boidsArray = new ArrayList<Boid>(arrayList);
+		boidSprite = new Sprite(defaultTexture);
+		for(Boid boid : arrayList){
+			//boidsArray.add(boid);
+			updateSpritePosition(boid);
+		}
+		/*System.out.println("boid array size: " + boidsArray.size());
+		boidSprite = new Sprite(defaultTexture);
 		for(Boid boid : arrayList){
 			if(!boidMap.containsKey(boid)){
 				boidMap.put(boid, new Sprite(defaultTexture));
-				updateSpritePosition(boid);
+				//updateSpritePosition(boid);
 			}
-		}
+		}*/
 	}
 	
 	public void updateSpritePosition(Boid boid){
 		//for(Iterator<Boid> boids = boidMap.keys(); boids.hasNext();){
 			Vector3 position = boid.getPosition();
-			boidMap.get(boid).setPosition(position.x, position.y);
+			boidSprite.setPosition(position.x, position.y);
+			//boidMap.get(boid).setPosition(position.x, position.y);
 		//}
 	}
 	
