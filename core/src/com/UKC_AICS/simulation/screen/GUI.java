62527d664750a54c5084a430ba9665203eec8fd8
James aka solf1re2 <jy1v07@gmail.com>
2014-07-02 11:08:41 +0100

    re did the damn GUI extraction from SimulationScreen

--- core/src/com/UKC_AICS/simulation/screen/GUI.java
+++ core/src/com/UKC_AICS/simulation/screen/GUI.java
@@ -1,9 +1,80 @@
 package com.UKC_AICS.simulation.screen;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
 /**
  * Created by James on 02/07/2014.
  */
 public class GUI {
 
+    SimulationScreen simScreen;
 
+    Stage stage;
+    Table table;
+    Skin skin;
+    Label fps;
+
+    public GUI (SimulationScreen ss) {
+        simScreen = ss;
+    }
+
+    /**
+     * setups up the UI.
+     */
+    void setStage() {
+        stage = new Stage(new ScreenViewport());
+        table = new Table();
+        table.debug();
+        skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+        fps = new Label(simScreen.getFPSString()+ simScreen.simulationManager.getTIme(), skin);
+        stage.addActor(table);
+
+        table.setFillParent(true);
+
+        // play/pause button
+        final TextButton playButton = new TextButton("Play", skin, "default");
+        playButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                if(simScreen.running){
+                    playButton.setText("Play");
+                }
+                else {
+                    playButton.setText("Pause");
+                }
+                simScreen.flipRunning();
+            }
+        });
+
+        //
+        final TextButton resetButton = new TextButton("Reset", skin, "default");
+        resetButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+                simScreen.simulationManager.reset();
+                simScreen.setup();
+                //TODO reset of simulation
+
+
+            }
+        });
+
+        //
+        TextArea console = new TextArea("console log",skin);
+
+        table.add(fps).bottom().left().expandY().width(500f).pad(0f, 10f, 10f, 0f);
+        table.add(playButton).size(100f, 30f).bottom().left().padLeft(20f).padBottom(10f);
+        table.add(resetButton).size(100f, 30f).expandX().bottom().left().padLeft(20f).padBottom(10f);
+        //
+        table.add(console).size(300f,30f).bottom();
+
+
+        Gdx.input.setInputProcessor(stage);
+    }
 }
