e74406f3d0ed8eee1531e090b41552ff8a5bc4dd
James Young aka solf1re2 <jy1v07@gmail.com>
2014-08-13 23:26:40 +0100

    staxparser method for objects added, TODO: assign individual bytes for objects for map

--- core/src/com/UKC_AICS/simulation/entity/ObjectData.java
+++ core/src/com/UKC_AICS/simulation/entity/ObjectData.java
@@ -15,20 +15,43 @@ public class ObjectData {
 //        this.subType = subType;
 //        this.imagePath = imagePath;
 //    }
+    public ObjectData() {
+    }
     public ObjectData(byte type, byte subType, String name) {
         this.type = type;
         this.subType = subType;
         this.name = name;
     }
+
     public byte getType() {
         return type;
+    }
+    public void setType(byte type) {
+        this.type = type;
     }
 
     public byte getSubType() {
         return subType;
     }
+    public void setSubType(byte subType) {
+        this.subType = subType;
+    }
 
-	public String getName() {
+    public String getName() {
         return name;
-	}
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+
+
+
 }
